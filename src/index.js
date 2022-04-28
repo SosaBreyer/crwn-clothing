af8679948fd0f18355b0aa2eb7fc3b70f965cb64
Javier Sosa Breyer <javier.sosabreyer@str8.de>
2022-04-28 14:32:16 +0200

    shopping cart

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
