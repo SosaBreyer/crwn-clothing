a4770c18c93f8fc3dfea050574efd54c47282313
Javier Sosa Breyer <javier.sosabreyer@str8.de>
2022-05-02 10:46:33 +0200

    added in addToCart feature

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,19 @@
+
+
+import './cart-item.styles.scss';
+
+const CartItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>{quantity} x ${price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
